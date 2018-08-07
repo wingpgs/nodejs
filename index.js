112fa12a0889fc6339c186e6e214530e5c7a96f5
GS Park <wingpgs@gmail.com>
2018-08-08 00:41:17 +0900

    mongodb connection test

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express  = require('express');
+var mongoose = require('mongoose');
+require('dotenv').config();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// CONNECT TO MONGODB SERVER
+mongoose.connect(process.env.MONGODB_URI);
+
+// DEFINE MODEL
+var Books = require('./models/books');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.get('/react', function(request, response) {
+  response.render('pages/react');
+});
+
+// GET ALL BOOKS
+app.get('/books', function(req,res){
+  Books.find(function(err, books){
+    if(err) return res.status(500).send({error: 'database failure'});
+    res.json(books);
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log( process.env.MONGODB_URI )
+  console.log('Node app is running on port', app.get('port'));
+});
